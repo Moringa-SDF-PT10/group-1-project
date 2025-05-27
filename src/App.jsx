188e4dde80c6c66568ed4ae3e30d52c52f435617
Vallary Awuor Onyando <valonyando@gmail.com>
2025-05-27 18:52:51 +0300

    Draft sign and log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Signin from "./components/Signin";
+import Login from "./components/Login";
 
 function App() {
 
 
   return (
-    <></>
+    <>
+      <Signin /> <Login />
+    </>
   )
 }
 
