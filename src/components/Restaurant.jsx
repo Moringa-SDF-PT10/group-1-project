9c93cef50dc63707ee3389870059e915f3118079
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-27 18:51:30 +0300

    initial commit

---
+++ src/components/Restaurant.jsx
@@ -0,0 +1,5 @@
+// Restaurants (restaurants list) (Elvis & Naima)
+    //Image of the restaurant
+    // Name name of the restaurant
+                // ||
+    // (Restaurant ) => name , image , cusines, menu, add to favouritesbutton,
