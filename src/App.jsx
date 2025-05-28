b7b0d930318c9fcab17c9086ca3ed112438d552b
Farouk Abdi <abdifarouk58@gmail.com>
2025-05-28 18:14:40 +0300

    added state at App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-
-
+const[restaurantData, setRestaurantData] = useState([])
+useEffect(()=>{
+  fetch("http://localhost:3000/restaurants")
+  .then(r=>r.json())
+  .then((data)=>setRestaurantData(data))
+},[])
   return (
-    <></>
+    <>
+
+    </>
   )
 }
 
