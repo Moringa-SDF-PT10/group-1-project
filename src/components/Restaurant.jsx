a0ea15998e8cfe7daa13ca454dcb101e6e4c5217
Farouk Abdi <abdifarouk58@gmail.com>
2025-05-30 19:02:54 +0300

    added routes

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -15,26 +15,12 @@ import RestauarntDetails from './RestaurantDetails';
                  <small>{address}</small>
                  <p>{cuisine}</p>
                  <span>{ratings}</span>
-                 
-                 {/* <ul>
-                  {menu.map((category, idx) => (
-                    <div className='menu' key={idx}>
-                      <li>{category.category}</li>
-                      <ul>
-                        {Array.isArray(category.foods) && category.foods.map((food, i) => (
-                          <li key={i}>{food}</li>
-                        ))}
-                      </ul>
-                      <img src={category.image} alt={category.category} className='menu-img' />
-                    </div>
-                  ))}
-                </ul> */}
                  <p>{hours}</p>
                  <p>{reviews}</p>
                  <button className='restauarant-button' onClick={onView}>View Restauarnt</button>
                  <img src={image}></img>
             </div>
-           
+
         )
 
     }
