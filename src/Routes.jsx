555421025c75b02cc2cdeb5023781fc07b898263
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-06-01 13:12:17 +0300

    minor fixes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,6 +8,7 @@ import RestaurantDetails from './components/RestaurantDetails';
 import Layout from './components/Layout';
 import FoodGallery from './components/FoodGallery';
 import ContactHelp from './components/ContactHelp';
+import Favourite from './components/FavouriteList';
 
 const Routes = [
   {
@@ -20,6 +21,7 @@ const Routes = [
       // { path: '/profile', element: <Profile /> },
       // { path: '/login', element: <Login /> },
       // { path: '/signin', element: <SignUp /> },
+      // {path: '/favorites' , element:<Favourite/>},
       {path :'/gallery', element:<FoodGallery/>},
       {path :'/support', element:<ContactHelp/>}
     ],
